b9897423fe910f72e06e74e85f30f451b00d4200
Praveensingh <126055101+praveensingh161290@users.noreply.github.com>
2023-08-21 10:27:53 +0530

    Bid Page Changes

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,22 +1,63 @@
 import React, { useEffect, useState } from "react";
 const Dashboard = () => {
+  const [sellerInput, setSellerInput] = useState({
+    make: "",
+    model: "",
+    year: "",
+    floorPrice: "",
+  });
   const url = "http://localhost:8080/api/cars?soldOut=false";
+  const bidStatus = "http://localhost:8080/api/bids/1/status";
+  const [data, setData] = useState([]);
+  const [vehicle, setVehicle] = useState({});
+  const [statusBid, setStatusBid] = useState([]);
   const getImage = () => {
     return "/images/product-" + Math.ceil(Math.random() * 6) + ".jpg";
   };
   const getKm = () => {
     return Math.ceil(Math.random() * 50000 + 10000) + "Kms";
   };
-  const [data, setData] = useState([]);
-  const [vehicle, setVehicle] = useState({});
   const fetchInfo = () => {
     return fetch(url)
       .then((res) => res.json())
       .then((d) => setData(d));
   };
-
+  const fetchStatusBid = () => {
+    return fetch(bidStatus)
+      .then((res) => res.json())
+      .then((d) => setStatusBid(d));
+  };
+  const handleSellerBid = (e) => {
+    const { name, value } = e.target;
+    setSellerInput((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      sellerInput?.floorPrice !== "" ||
+      sellerInput?.make !== "" ||
+      sellerInput?.model !== "" ||
+      sellerInput?.year !== ""
+    ) {
+      console.log(sellerInput);
+      setSellerInput({
+        make: "",
+        model: "",
+        year: "",
+        floorPrice: "",
+      });
+    } else {
+      alert("Please fill all the details");
+    }
+  };
   useEffect(() => {
     fetchInfo();
+    fetchStatusBid();
   }, []);
 
   return (
@@ -25,7 +66,7 @@ const Dashboard = () => {
         <ul class="nav nav-tabs">
           <li class="active">
             <a href="#1" data-toggle="tab">
-              Buyers
+              Car Lists
             </a>
           </li>
           <li>
@@ -33,8 +74,14 @@ const Dashboard = () => {
               Sellers
             </a>
           </li>
+          <li>
+            <a href="#3" data-toggle="tab">
+              Buyers Data
+            </a>
+          </li>
         </ul>
         <div class="tab-content ">
+          {/******* Car Lists Tab *********/}
           <div class="tab-pane active" id="1">
             <div className="cars-list">
               {data?.map((obj, index) => {
@@ -86,9 +133,10 @@ const Dashboard = () => {
               })}
             </div>
           </div>
+          {/******* Seller Tab *********/}
           <div class="tab-pane" id="2">
             <div className="seller-form">
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div class="form-group">
                   <label for="recipient-name" class="col-form-label">
                     Make
@@ -96,9 +144,10 @@ const Dashboard = () => {
                   <input
                     type="text"
                     class="form-control"
-                    id="car-id"
-                    name="car_id"
-                    value={""}
+                    id="make"
+                    name="make"
+                    value={sellerInput.make}
+                    onChange={handleSellerBid}
                   />
                 </div>
                 <div class="form-group">
@@ -108,9 +157,10 @@ const Dashboard = () => {
                   <input
                     type="text"
                     class="form-control"
-                    id="buyer-id"
-                    name="buyer_id"
-                    value={""}
+                    id="model"
+                    name="model"
+                    value={sellerInput.model}
+                    onChange={handleSellerBid}
                   />
                 </div>
                 <div class="form-group">
@@ -120,9 +170,10 @@ const Dashboard = () => {
                   <input
                     type="text"
                     class="form-control"
-                    id="bid-amount"
-                    name="bid_amount"
-                    value={""}
+                    id="year"
+                    name="year"
+                    value={sellerInput.year}
+                    onChange={handleSellerBid}
                   />
                 </div>
                 <div class="form-group">
@@ -132,16 +183,163 @@ const Dashboard = () => {
                   <input
                     type="text"
                     class="form-control"
-                    id="max-bid-amount"
-                    name="max_bid_amount"
-                    value={""}
+                    id="floorPrice"
+                    name="floorPrice"
+                    value={sellerInput.floorPrice}
+                    onChange={handleSellerBid}
                   />
                 </div>
                 <button className="button-style">List for Bidding</button>
               </form>
             </div>
           </div>
+          {/******* Bid Status Tab *********/}
+          <div class="tab-pane" id="3">
+            <h2 className="total-bids">
+              Total Bids : <span>4</span>
+            </h2>
+            <div className="status-bids">
+              <div className="row ">
+                <div className="col-md-4">
+                  <div className="bid-status">
+                    <ul>
+                      <li status-data="all">All Bids</li>
+                      <li status-data="approved">
+                        Approved Bids <span>(1)</span>
+                      </li>
+                      <li status-data="pending">
+                        Pending Bids <span>(2)</span>
+                      </li>
+                      <li status-data="rejected">
+                        Rejected Bids <span>(1)</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="cars-list">
+                    {statusBid?.map((data, index) => {
+                      return (
+                        <div
+                          className="single-card"
+                          data-status="pending"
+                          key={index}
+                        >
+                          <div className="car-section">
+                            <div className="image-col">
+                              <img src={getImage()} alt="Img" />
+                            </div>
+                            <div className="car-details-col">
+                              <h2>{data.make + " " + data.model}</h2>
+                              <h3>{data.year}</h3>
+                              <ul>
+                                <li>{getKm()}</li>
+                              </ul>
+                              <h4 className="pending">
+                                Status : <span>Pending</span>
+                              </h4>
+                            </div>
+                          </div>
+                          <div className="bid-section">
+                            <div className="left-col">
+                              <h3>Price</h3>
+                              <h2>
+                                <strong>$13000</strong>
+                              </h2>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+
+                    <div className="single-card" data-status="approved">
+                      <div className="car-section">
+                        <div className="image-col">
+                          <img src={getImage()} alt="Img" />
+                        </div>
+                        <div className="car-details-col">
+                          <h2>Hyndai Elantra </h2>
+                          <h3>2015</h3>
+                          <ul>
+                            <li>33,637 km</li>
+                            <li>| Car</li>
+                            <li>| 2dr Cpe Carrera 4</li>
+                          </ul>
+                          <h4 className="approved">
+                            Status : <span>Approved</span>
+                          </h4>
+                        </div>
+                      </div>
+                      <div className="bid-section">
+                        <div className="left-col">
+                          <h3>Price</h3>
+                          <h2>
+                            <strong>$14000</strong>
+                          </h2>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="single-card" data-status="rejected">
+                      <div className="car-section">
+                        <div className="image-col">
+                          <img src={getImage()} alt="Img" />
+                        </div>
+                        <div className="car-details-col">
+                          <h2>Hyndai Verna</h2>
+                          <h3>2014</h3>
+                          <ul>
+                            <li>33,637 km</li>
+                            <li>| Car</li>
+                            <li>| 2dr Cpe Carrera 4</li>
+                          </ul>
+                          <h4 className="rejected">
+                            Status : <span>Rejected</span>
+                          </h4>
+                        </div>
+                      </div>
+                      <div className="bid-section">
+                        <div className="left-col">
+                          <h3>Price</h3>
+                          <h2>
+                            <strong>$15000</strong>
+                          </h2>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="single-card" data-status="pending">
+                      <div className="car-section">
+                        <div className="image-col">
+                          <img src={getImage()} alt="Img" />
+                        </div>
+                        <div className="car-details-col">
+                          <h2>Hyndai Creta</h2>
+                          <h3>2014</h3>
+                          <ul>
+                            <li>33,637 km</li>
+                            <li>| Car</li>
+                            <li>| 2dr Cpe Carrera 4</li>
+                          </ul>
+                          <h4 className="pending">
+                            Status : <span>Pending</span>
+                          </h4>
+                        </div>
+                      </div>
+                      <div className="bid-section">
+                        <div className="left-col">
+                          <h3>Price</h3>
+                          <h2>
+                            <strong>$16000</strong>
+                          </h2>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        {/******* Popup Modal For Bidding *********/}
         <div
           class="modal fade"
           id="exampleModal"
@@ -175,7 +373,7 @@ const Dashboard = () => {
                       type="text"
                       class="form-control"
                       id="car-id"
-                      name="car_id"
+                      name="carId"
                       value={vehicle.id}
                     />
                   </div>
